daa8811076e9dfb131961a390d63f2b44abb2866
alex.mm <103150113+aleeme@users.noreply.github.com>
2022-05-12 21:13:16 +0900

    Update main.c

--- main.c
+++ main.c
@@ -26,27 +26,52 @@ int main(){
     datecmp();
     timecmp();
 }
-
 //파일 1의 정보를 가져오는 함수 작성
 void filestat1(){
-    
-}
+    char* filename1 = "text1";
+    if(stat(filename1, &stat1) != 0){
+	    switch(errno){
+		    case ENOENT:
+			    fprintf(stderr, "file1 not found. \n"); break;
+		    case EINVAL:
+			    fprintf(stderr, "invalid parameter to stat. \n"); break;
+		    default:
+			    fprintf(stderr, "unexpected error. \n");
+	    }
+	}
 
+}
 //파일 2의 정보를 가져오는 함수 작성
 void filestat2(){
+    char* filename2 = "text2";
+    if(stat(filename2, &stat2) != 0){
+	    switch(errno){
+		    case ENOENT:
+			    fprintf(stderr, "file1 not found. \n"); break;
+		    case EINVAL:
+			    fprintf(stderr, "invalid parameter to stat. \n"); break;
+		    default:
+			    fprintf(stderr, "unexpected error. \n");
+	    }
     
+    }
 }
 
 //파일 1의 시간 정보를 가져오는 함수 작성
 void filetime1(){
-    
+
+	time1 = malloc(sizeof(struct tm));
+
+	localtime_r(&stat1.st_mtime, time1);
 }
 
 //파일 2의 시간 정보를 가져오는 함수 작성
 void filetime2(){
-    
-}
 
+	time2 = malloc(sizeof(struct tm));
+	localtime_r(&stat2.st_mtime, time2);
+
+}
 //두 개의 파일 크기를 비교하는 함수 작성
 void sizecmp(){
     
